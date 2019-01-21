5a00eed4dcc467b03940da960f487cb1266f887b
JF Padilla <emailreel@gmail.com>
2019-01-21 12:17:36 -0500

    initialize subject notes project
    + add class definitions for Python ORM: SQLAlchemy
    
    This is the second project toward a Full-Stack Nanodegree from Udacity.

---
+++ database_setup.py
@@ -0,0 +1,32 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Topic(Base):
+    __tablename__ = 'topic'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+class Section(Base):
+    __tablename__ = 'section'
+    name = Column(String(80), nullable = False)
+    description = Column(String(250))
+    id = Column(Integer, primary_key = True)
+    topic_id = Column(Integer, ForeignKey('topic.id'))
+    topic = relationship(Topic)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+        }
+
+engine = create_engine('sqlite:///subjectNotes.db')
+Base.metadata.create_all(engine)
+# EOF
