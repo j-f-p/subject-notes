1fee1d64646da753f63cc80c7a91c879c1850d16
JF Padilla <emailreel@gmail.com>
2019-05-19 20:13:38 -0400

    refactor: limit section to 1 editor in db init
    feat:
    + Import Editor.
    + Add initial editors.
    * Have a constant section title width in database initialization output.
    
    refactor:
    * Assign editor to section by section's editor_id property.
    - Remove any second editor.

--- initSubjectNotesDB.py
+++ initSubjectNotesDB.py
@@ -2,7 +2,7 @@ from os import environ
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Topic, Section
+from database_setup import Base, Topic, Editor, Section
 
 
 if environ.get('DATABASE_URL') is None:
@@ -16,6 +16,10 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
+
+# The Initial Editors
+session.add_all([Editor(email="editor@example.com"),  # default editor
+                 Editor(email="emailreel@gmail.com")])
 
 # Deep Learning Topics
 session.add_all([Topic(title="Historical Trends"),
@@ -36,25 +40,25 @@ session.commit()
 #     initialized for each topic is limited.
 session.add_all([
     Section(id=10, title="Intro", topic_id=1),
-    Section(id=11,   title="Changing Names", topic_id=1),
-    Section(id=12,   title="Increasing Data Set", topic_id=1),
-    Section(id=13,   title="Increasing Model Size", topic_id=1),
-    Section(id=14,   title="Increasing Accuracy", topic_id=1),
+    Section(id=11,   title="Changing Names", topic_id=1, editor_id=2),
+    Section(id=12,   title="Increasing Data Set", topic_id=1, editor_id=2),
+    Section(id=13,   title="Increasing Model Size", topic_id=1, editor_id=2),
+    Section(id=14,   title="Increasing Accuracy", topic_id=1, editor_id=2),
     Section(id=20, title="Intro", topic_id=2),
-    Section(id=21,   title="Variables", topic_id=2),
-    Section(id=22,   title="Operations", topic_id=2),
-    Section(id=23,   title="Linear Dependence", topic_id=2),
+    Section(id=21,   title="Variables", topic_id=2, editor_id=2),
+    Section(id=22,   title="Operations", topic_id=2, editor_id=2),
+    Section(id=23,   title="Linear Dependence", topic_id=2, editor_id=2),
     Section(id=30, title="Intro", topic_id=3),
-    Section(id=31,   title="Random Variables", topic_id=3),
-    Section(id=32,   title="Distributions", topic_id=3),
-    Section(id=33,   title="Probability Types", topic_id=3),
+    Section(id=31,   title="Random Variables", topic_id=3, editor_id=2),
+    Section(id=32,   title="Distributions", topic_id=3, editor_id=2),
+    Section(id=33,   title="Probability Types", topic_id=3, editor_id=2),
     Section(id=40, title="Intro", topic_id=4),
-    Section(id=41,   title="Numerical Error", topic_id=4),
-    Section(id=42,   title="Conditioning", topic_id=4),
+    Section(id=41,   title="Numerical Error", topic_id=4, editor_id=2),
+    Section(id=42,   title="Conditioning", topic_id=4, editor_id=2),
     Section(id=50, title="Intro", topic_id=5),
-    Section(id=51,   title="The Task", topic_id=5),
-    Section(id=52,   title="Performance Measure", topic_id=5),
-    Section(id=53,   title="Experience", topic_id=5)])
+    Section(id=51,   title="The Task", topic_id=5, editor_id=2),
+    Section(id=52,   title="Performance Measure", topic_id=5, editor_id=2),
+    Section(id=53,   title="Experience", topic_id=5, editor_id=2)])
 # Above, the last section id is 53. The actual number of sections is less.
 session.commit()
 
@@ -70,7 +74,6 @@ section.notes = ' '.join('''
     march of this history. Simultaneously, deep learning mathematical model
     sizes have continued to increase. Logically, the accuracy of analyses with
     deep learning have continued to improve.'''.split())
-section.editor = "emailreel@gmail.com"
 section.utce = datetime.utcnow()
 session.commit()
 
@@ -79,7 +82,6 @@ section.notes = ' '.join('''
     What we now know as deep learning was introduced as cybernetics circa 1940.
     It began being called connectionism or neural networks circa 1980. The rise
     of the current name began circa 2006.'''.split())
-section.editor = "emailreel@gmail.com"
 section.utce = datetime.utcnow()
 session.commit()
 
@@ -88,7 +90,6 @@ section.notes = ' '.join('''
     Increases in computer memory have resulted in commensurate increases in
     digital data. Larger data sets have enabled deep learning algorithms to be
     applied to increasingly complex applications.'''.split())
-section.editor = "emailreel@gmail.com"
 section.utce = datetime.utcnow()
 session.commit()
 
@@ -99,7 +100,6 @@ section.notes = ' '.join('''
     artificial neural network has doubled about every 2.5 years since their
     inception.  Additionally, the number of connections per model neuron has
     risen.'''.split())
-section.editor = "emailreel@gmail.com"
 section.utce = datetime.utcnow()
 session.commit()
 
@@ -107,7 +107,6 @@ section = session.query(Section).filter_by(id=14).one()
 section.notes = ' '.join('''
     Image recognition error rate steadily dropped annually from 28% in 2010 to
     4% in 2015.'''.split())
-section.editor = "emailreel@gmail.com"
 section.utce = datetime.utcnow()
 session.commit()
 
@@ -116,7 +115,7 @@ for topic in topics:
     print("{} {}".format(topic.id, topic.title))
     sections = session.query(Section).filter_by(topic_id=topic.id)
     for section in sections:
-        print("\t{} {} {} UTC".format(section.id, section.title,
+        print("\t{} {:24} {} UTC".format(section.id, section.title,
               section.utci.strftime('%Y-%m-%d %-I:%M:%S.%f %p')))
 # section.utci is not aware of timezone. Thus, '%Z' format would return empty.
 # However, it is known to have been generated in UTC. Thus, 'UTC' can be added
